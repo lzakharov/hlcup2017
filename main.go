57e4b237162cb963e15c37510ab525da4bb7244f
Lev Zakharov <l.j.zakharov@gmail.com>
2018-07-08 15:27:15 +0300

    Add main.go with defined API routes

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/users/{id:[0-9]+}", nil).Methods("GET")
+	router.HandleFunc("/users/{id:[0-9]+}/visits", nil).Methods("GET")
+	router.HandleFunc("/users/{id:[0-9]+}", nil).Methods("POST")
+	router.HandleFunc("/users/new", nil).Methods("POST")
+
+	router.HandleFunc("/locations/{id:[0-9]+}", nil).Methods("GET")
+	router.HandleFunc("/locations/{id:[0-9]+}/avg", nil).Methods("GET")
+	router.HandleFunc("/locations/{id:[0-9]+}", nil).Methods("POST")
+	router.HandleFunc("/locations/new", nil).Methods("POST")
+
+	router.HandleFunc("/visits/{id:[0-9]+}", nil).Methods("GET")
+	router.HandleFunc("/visits/{id:[0-9]+}", nil).Methods("POST")
+	router.HandleFunc("/visits/new", nil).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
